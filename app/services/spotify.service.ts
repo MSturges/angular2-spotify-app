ce11715b7df3145728a1ad905314ec3e084af606
MSturges <Sturges3232@gmail.com>
2016-12-20 10:37:46 -0700

    displaying albums

--- app/services/spotify.service.ts
+++ app/services/spotify.service.ts
@@ -6,7 +6,8 @@ import 'rxjs/add/operator/map';
 export class SpotifyService {
 
   private searchUrl: string;
-
+  private artistUrl: string;
+  private albumsUrl: string;
 
   constructor(private _http:Http){
 
@@ -15,6 +16,19 @@ export class SpotifyService {
   searchMusic(str:string, type='artist') {
     this.searchUrl = 'https://api.spotify.com/v1/search?query='+str+'&offset=0&limit=20&type='+type+'&market=us';
     return this._http.get(this.searchUrl)
-      .map(res => res.json())
+    .map(res => res.json())
   }
+
+  getArtist(id:string) {
+    this.artistUrl = 'https://api.spotify.com/v1/artists/' + id;
+    return this._http.get(this.artistUrl)
+    .map(res => res.json())
+  }
+
+  getAlbums(artistId:string){
+    this.albumsUrl = 'https://api.spotify.com/v1/artists/' + artistId + '/albums';
+    return this._http.get(this.albumsUrl)
+    .map(res => res.json())
+  }
+
 }
